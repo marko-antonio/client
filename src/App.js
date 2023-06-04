da77776e2acdd724b1fe3db7920c71fda8ae22d3
marko-antonio <mark.a.racuya@gmail.com>
2023-06-04 16:57:26 +0800

    may changes ako

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import axios from 'axios';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+const [kalbo ,setKalbo] = useState([]);
+
+useEffect(() => {
+  axios.get("http://localhost:5010/api/user/").then((myvariable)=>{
+    setKalbo(myvariable.data);
+  })
+  
+}, []);
+
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className="App">{kalbo.map((value,key)=>{
+    return(
+      <div>{value.username}</div>
+    )
+    })}
     </div>
   );
 }
