0afa278d97591bc776784195daffe53b3b6e2949
marko-antonio <mark.a.racuya@gmail.com>
2023-06-14 10:44:55 +0800

    with dashboard na
    patulong ako identify ng mga database na kailangan idisplay sa dashboard beh

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,10 +1,103 @@
+import { Button, Card, CardActions, CardContent, Stack, Typography } from '@mui/material'
 import React from 'react'
 
 function Dashboard() {
   return (
-    <div>
-      <h1>This is DashBoard</h1>
-    </div>
+    <Stack
+      spacing={{ xs: 1, sm: 2 }}
+      direction="row"
+      useFlexGap
+      flexWrap="wrap"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Card sx={{ minWidth: 275 }}>
+        <CardContent>
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+            Thesis Request
+          </Typography>
+          <Typography sx={{ fontSize: 18 }}>
+            <h2>4</h2>
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">View more</Button>
+        </CardActions>
+      </Card>
+
+      <Card sx={{ minWidth: 275 }}>
+        <CardContent>
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+            Schedule Request
+          </Typography>
+          <Typography sx={{ fontSize: 18 }}>
+            <h2>10</h2>
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">View more</Button>
+        </CardActions>
+      </Card>
+
+      <Card sx={{ minWidth: 275 }}>
+        <CardContent>
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+            Account Request
+          </Typography>
+          <Typography sx={{ fontSize: 18 }}>
+            <h2>4</h2>
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">View more</Button>
+        </CardActions>
+      </Card>
+
+      <Card sx={{ minWidth: 275 }}>
+        <CardContent>
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+            Total Group
+          </Typography>
+          <Typography sx={{ fontSize: 18 }}>
+            <h2>4</h2>
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">View more</Button>
+        </CardActions>
+      </Card>
+
+      <Card sx={{ minWidth: 275 }}>
+        <CardContent>
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+            Tangina
+          </Typography>
+          <Typography sx={{ fontSize: 18 }}>
+            <h2>4</h2>
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">View more</Button>
+        </CardActions>
+      </Card>
+
+      <Card sx={{ minWidth: 275 }}>
+        <CardContent>
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+            Mo
+          </Typography>
+          <Typography sx={{ fontSize: 18 }}>
+            <h2>4</h2>
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">View more</Button>
+        </CardActions>
+      </Card>
+    </Stack>
+
+
+
   )
 }
 
